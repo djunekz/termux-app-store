36c96a19ae16f2de68d69a3d9d1a610c9b966ee1
droidv1 <zjunekz@gmail.com>
2026-02-12 01:42:42 +0700

    New pkg: ghostrack useful tool to track location or mobile number

---
+++ packages/ghostrack/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/HunxByts/GhostTrack
+TERMUX_PKG_DESCRIPTION="Useful tool to track location or mobile number"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-app-store"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/droidv1/termool/releases/download/v${TERMUX_PKG_VERSION}/ghostrack.tar.gz
+TERMUX_PKG_SHA256=93f935f38efffc574c44f2c73e2ef90064747efed92c0765b0fd7f1bc07ea662
+TERMUX_PKG_DEPENDS=""
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make_install() {
+  install -Dm755 ghostrack.py $TERMUX_PREFIX/bin/ghostrack
+}
