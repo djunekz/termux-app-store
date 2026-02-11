59f003613444879113581a675a0aa355881f541c
droidv1 <zjunekz@gmail.com>
2026-02-12 02:40:42 +0700

    New pkg: ghostrack

--- packages/ghostrack/build.sh
+++ packages/ghostrack/build.sh
@@ -5,7 +5,6 @@ TERMUX_PKG_MAINTAINER="@termux-app-store"
 TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/droidv1/termool/releases/download/v${TERMUX_PKG_VERSION}/ghostrack.tar.gz
 TERMUX_PKG_SHA256=93f935f38efffc574c44f2c73e2ef90064747efed92c0765b0fd7f1bc07ea662
-TERMUX_PKG_DEPENDS=""
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make_install() {
