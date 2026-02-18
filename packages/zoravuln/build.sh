bd83bc2da39c5de1960ff5df8857d0f3d97f71f2
droidv1 <zjunekz@gmail.com>
2026-02-19 04:55:23 +0700

    New pkg: zoravuln
    * docs: auto-update CHANGELOG.md
    
    * docs: auto-update CHANGELOG.md
    
    * New pkg: zoravuln
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ packages/zoravuln/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/djunekz/termux-app-store
+TERMUX_PKG_DESCRIPTION="Ultimate scanner vulnerability"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-app-store"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/droidv1/termool/releases/download/v${TERMUX_PKG_VERSION}/zoravuln
+TERMUX_PKG_SHA256=e8f3bb069cdd9afbc238c5d55ca0046b35e853590af061261e2d95e4cc8ec89e
+TERMUX_PKG_BUILD_IN_SRC=true
