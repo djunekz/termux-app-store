a3872b38eadf6a5ff0324b80e3cc132a6bdab186
djunekz <gab288.gab288@passinbox.com>
2026-02-19 15:03:55 +0700

    Update pkg: ghostrack

--- packages/ghostrack/build.sh
+++ packages/ghostrack/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Useful tool to track location or mobile number"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-app-store"
 TERMUX_PKG_VERSION=1.0.0
-TERMUX_PKG_SRCURL=https://github.com/droidv1/termool/releases/download/v${TERMUX_PKG_VERSION}/ghostrack.tar.gz
+TERMUX_PKG_SRCURL=https://github.com/djunekz/archive/releases/download/v${TERMUX_PKG_VERSION}/ghostrack.tar.gz
 TERMUX_PKG_SHA256=43c5fe4734ef358fe17659a08b5846682d43f046e880233503f7412ebbdf1238
 TERMUX_PKG_BUILD_IN_SRC=true
 
