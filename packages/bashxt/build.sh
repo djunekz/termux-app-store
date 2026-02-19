c76365fb59f92ed356b1f057b711b6db1b53f139
djunekz <gab288.gab288@passinbox.com>
2026-02-19 16:33:02 +0700

    New pkg: bashxt

---
+++ packages/bashxt/build.sh
@@ -0,0 +1,8 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/djunekz/bashxt
+TERMUX_PKG_DESCRIPTION="basic command, code color, shortcut keyboar, etc information"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-app-store"
+TERMUX_PKG_VERSION=2.2
+TERMUX_PKG_SRCURL=https://github.com/djunekz/bashxt/releases/download/v${TERMUX_PKG_VERSION}/bashxt.tar.gz
+TERMUX_PKG_SHA256=b9b27f232d6a752f3243e915d98bb03ecbd982f37e45243d7b82905f745e9512
+TERMUX_PKG_BUILD_IN_SRC=true
